8e5e3cbdcb2a157a404c8c5f1cd4f565aca88425
KryNak <krystian.nakielski200397@gmail.com>
2022-05-08 22:14:41 +0200

    Added save logic to preview dialog.

--- app/src/main/java/pl/edu/todo/fragments/PreviewFragment.kt
+++ app/src/main/java/pl/edu/todo/fragments/PreviewFragment.kt
@@ -5,6 +5,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.DialogFragment
+import pl.edu.todo.adapters.TodosAdapter
+import pl.edu.todo.data.DataSource
 import pl.edu.todo.databinding.FragmentPreviewBinding
 import pl.edu.todo.listeners.OnPreviewProgressChangeListener
 import pl.edu.todo.model.Todo
@@ -34,8 +36,24 @@ class PreviewFragment(
         binding.deadlineField.setText(dateFormatter.format(todo.deadline))
         binding.progressField.progress = todo.progress
         binding.statsProgressbar.progress = todo.progress
+        binding.percentageProgressLabel.text = todo.progress.toString()
 
-        binding.progressField.setOnSeekBarChangeListener(OnPreviewProgressChangeListener(binding.statsProgressbar))
+        binding.progressField.setOnSeekBarChangeListener(
+            OnPreviewProgressChangeListener(binding.statsProgressbar, binding.percentageProgressLabel)
+        )
+
+        binding.saveBtn.setOnClickListener {
+            val index = DataSource.todos.indexOf(todo)
+            DataSource.todos[index] = Todo(
+                todo.taskName,
+                todo.priority,
+                binding.statsProgressbar.progress,
+                todo.deadline
+            )
+
+            TodosAdapter.adapter.replace(DataSource.todos)
+            dismiss()
+        }
 
     }
 
--- app/src/main/java/pl/edu/todo/listeners/OnPreviewProgressChangeListener.kt
+++ app/src/main/java/pl/edu/todo/listeners/OnPreviewProgressChangeListener.kt
@@ -2,13 +2,16 @@ package pl.edu.todo.listeners
 
 import android.widget.ProgressBar
 import android.widget.SeekBar
+import android.widget.TextView
 
 class OnPreviewProgressChangeListener(
-    private val statsProgressbar: ProgressBar
+    private val statsProgressbar: ProgressBar,
+    private val percentageLabel: TextView
 ) : SeekBar.OnSeekBarChangeListener {
 
     override fun onProgressChanged(p0: SeekBar?, progress: Int, p2: Boolean) {
         statsProgressbar.progress = progress
+        percentageLabel.text = progress.toString()
     }
 
     override fun onStartTrackingTouch(p0: SeekBar?) {
@@ -18,6 +21,7 @@ class OnPreviewProgressChangeListener(
     override fun onStopTrackingTouch(seekBar: SeekBar?) {
         seekBar?.let {
             statsProgressbar.progress = it.progress
+            percentageLabel.text = it.progress.toString()
         }
     }
 
